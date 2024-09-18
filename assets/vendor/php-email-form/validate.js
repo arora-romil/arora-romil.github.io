50bac474406408116c28507fbbb5926b1eacb1b2
Romil Arora <romil@Romils-MacBook-Air.local>
2024-09-19 02:03:14 +0530

    Initial Commit

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,12 @@
+(function () {
+  "use strict";
+  let forms = document.querySelectorAll('.php-email-form');
+  forms.forEach( function(e) {
+    e.addEventListener('submit', function(event) {
+      let thisForm = this;
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+    });
+  });
+})();
